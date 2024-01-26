479f80bc23c1a2accaf28157d0ee322733835766
Gustavo Mendes <gustamms@hotmail.com>
2024-01-26 20:26:56 -0300

    Feature: Verificacao de usuario na base de dados

--- app/http/controllers/user_controller.go
+++ app/http/controllers/user_controller.go
@@ -35,7 +35,7 @@ func (r *UserController) Store(ctx http.Context) http.Response {
 	password := ctx.Request().Input("password")
 
 	user := models.User{Name: name, Username: username, Password: password}
-	facades.Orm().Query().Create(&user)
+	facades.Orm().Query().Where("username", username).FirstOrCreate(&user, user)
 
 	return ctx.Response().Status(201).Json(http.Json{
 		"message": &user,
@@ -46,7 +46,13 @@ func (r *UserController) Show(ctx http.Context) http.Response {
 	var user models.User
 	id := ctx.Request().Input("id")
 
-	facades.Orm().Query().FindOrFail(&user, id)
+	err := facades.Orm().Query().FindOrFail(&user, id)
+
+	if err != nil {
+		return ctx.Response().Status(404).Json(http.Json{
+			"message": "Id não encontrado na base de dados",
+		})
+	}
 
 	return ctx.Response().Success().Json(http.Json{
 		"data": &user,
